f5a04fd5353012464a10537286fb2a6788565fd0
Josephdada01 <ridwandada500@gmail.com>
2024-05-18 16:58:18 +0100

    learning webpack

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
 
 module.exports = {
   entry: './src/index.js', // Your entry point
   output: {
-    filename: 'main.js', // The output file name
+    filename: 'bundle.js', // The output file name
     path: path.resolve(__dirname, 'dist'), // The output directory
   },
   mode: 'development', // Set the mode to 'development' or 'production'
@@ -17,6 +20,47 @@ module.exports = {
           options: {
             presets: ['@babel/preset-env'], // Use the preset-env preset
           },
+        },
+      },
+      {
+        test: /\.css$/, // Apply this rule to .css files
+        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
         },
       },
     ],
